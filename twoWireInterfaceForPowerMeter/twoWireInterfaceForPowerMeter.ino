adff03b7db3e17cde4e6c87629bf8bff9f34ae50
Odonata <github@3-1415.de>
2017-07-25 22:35:34 +0200

    added some more comments, no other changes

--- twoWireInterfaceForPowerMeter/twoWireInterfaceForPowerMeter.ino
+++ twoWireInterfaceForPowerMeter/twoWireInterfaceForPowerMeter.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+/* Here is the S0 interface connected to*/
 #define S0 14 
 
 #define DEBUG 1
@@ -8,6 +9,7 @@
 const char* ssid     = <Syntax error because of missing ssid>;
 const char* password = <Syntax error because of missing pw>;
 
+/* change the Port and IP of the influxdb connector */               
 int port = 8089;
 byte host[] = {192, 168, 178, 55};
 
@@ -18,14 +20,14 @@ void setup(){
     Serial.begin(9600);
     Serial.println("Setup");
     #endif
-    
+
+    /*Set the Pin As input and define a interrupt for a rising signal*/
     pinMode(S0, INPUT);
     attachInterrupt(S0, highInterrupt, RISING);
     setupWiFi();
-    
-
 }
 
+/*handle the interrupt and define on for a Falling signal*/
 void highInterrupt(){
     detachInterrupt(S0);
     attachInterrupt(S0, lowInterrupt, FALLING);
@@ -36,6 +38,7 @@ void highInterrupt(){
     
 }
 
+/*handle the Interrupt and define again for a rising signal also we need to send a message.*/
 void lowInterrupt(){
     detachInterrupt(S0);
     attachInterrupt(S0,  highInterrupt, RISING);
@@ -66,7 +69,7 @@ void setupWiFi(){
   #endif
 }
 
-
+/*send out a message to the influxdb which should be configured to fulfil the rest of the stuff*/
 void sendMessageToServer(){
    String content = "powertick value=1";
    udp.beginPacket(host, port);
